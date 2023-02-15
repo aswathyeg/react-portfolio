4f649e051662fa444b424674f632fb8278311e38
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-02-15 14:38:39 +0530

    added bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,93 +10,92 @@ import {
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
+import { Container, Navbar, NavbarBrand, NavItem, Nav } from "react-bootstrap";
 
-const Navbar = () => {
+const NavbarPage = () => {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-      <div>
-        <h1 className=" font-thin text-2xl italic font-serif">TB</h1>
-      </div>
-      {/* menu */}
-      <ul className="hidden md:flex gap-x-8">
-        <li>
-          <Link to="home" smooth={true} duration={500}>
+    <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+      <Container variant="dark" bg="dark" style={{ height: 80 }}>
+        <NavbarBrand>Aswathy E G </NavbarBrand>
+
+        {/* menu */}
+        <Nav className="m-auto">
+          <NavItem Link to="home">
             Home
-          </Link>
-        </li>
-        <li>
-          <Link to="about" smooth={true} duration={500}>
+          </NavItem>
+          <NavItem Link to="about">
             About
-          </Link>
-        </li>
-        <li>
-          <Link to="skills" smooth={true} duration={500}>
+          </NavItem>
+          <NavItem Link to="skills">
+            {" "}
             Skills
-          </Link>
-        </li>
-        <li>
-          <Link to="work" smooth={true} duration={500}>
+          </NavItem>
+
+          <NavItem Link to="work">
+            {" "}
             Work
-          </Link>
-        </li>
-        <li>
-          <Link to="contact" smooth={true} duration={500}>
+          </NavItem>
+          <NavItem Link to="contact">
+            {" "}
             Contact
-          </Link>
-        </li>
-      </ul>
-      {/* Hamburger */}
-      <div onClick={handleClick} className="md:hidden z-10">
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-      {/* Mobile menu */}
-      <ul
-        className={
-          !nav
-            ? "hidden"
-            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
-        }
-      >
-        <li className="py-6 text-4xl">
-          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
-            Home
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          {" "}
-          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
-            About
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          {" "}
-          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
-            Skills
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          {" "}
-          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
-            Work
-          </Link>
-        </li>
-        <li className="py-6 text-4xl">
-          {" "}
-          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+          </NavItem>
+        </Nav>
+        {/* Hamburger */}
+
+        {/* Mobile menu */}
+        {/* <ul>
+          <li>
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+              Work
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul> */}
+      </Container>
+
       {/* Social icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
               className="flex justify-between items-center w-full text-gray-300"
-              href="/"
+              href="/https://www.linkedin.com/in/aswathyeg/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
@@ -127,7 +126,7 @@ const Navbar = () => {
           </li>
         </ul>
       </div>
-    </div>
+    </Navbar>
   );
 };
-export default Navbar;
+export default NavbarPage;
