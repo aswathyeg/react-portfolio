e31bdabebbe65d76b1140e51f83916f2a3d3ecf5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-02-15 14:39:26 +0530

    updated text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
-import me from "../assets/me.png";
+import photo from "../assets/photo.jpg";
 import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-[#0a192f]">
+    <div name="home" className="h-screen w-full bg-[#0d195f]">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Web Developer
+            I'm a Front End Developer.
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I have 4 years of experience in graphics design and web development.
-            Currently, I love to work on web application using technologies like
-            React, Tailwind, Next.js and Mongodb.
+            Over the past 2 years experience in front end development, I have
+            done a handful of projects in React. I am doing reguler cotributions
+            to Github.Sharing my technical knowledge in Medium is my other
+            interest.
           </p>
           <div>
             <Link
@@ -32,9 +33,9 @@ const Home = () => {
         </div>
         <div>
           <img
-            src={me}
+            src={photo}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-2/3"
           />
         </div>
       </div>
