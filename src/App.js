23d92ef07852d09cb7c149387ee2716bad91b983
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-02-17 12:37:47 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
-
+// import "./App.css";
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <Works />
-      <Contact />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="works" element={<Works />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
