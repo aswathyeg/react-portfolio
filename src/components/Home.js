55c701ee8bf62cd664b232f679e0ec32252120a9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-19 22:02:56 +0530

    updated navlink position

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import aswathy from "../assets/aswathy.jpg";
-
 import { Container } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
@@ -16,6 +15,13 @@ import Works from "./Works";
 import About from "./About";
 import MoreAbout from "./MoreAbout";
 const Home = () => {
+  // const handleClickScroll = () => {
+  //   const element = document.getElementById("section-1");
+  //   if (element) {
+  //     // 👇 Will scroll smoothly to the top of the next section
+  //     element.scrollIntoView({ behavior: "smooth" });
+  //   }
+  // };
   return (
     <div className="homeDiv">
       <Nav className="flex-column">
@@ -39,15 +45,35 @@ const Home = () => {
       </Nav>
       <Container className="homeContainer">
         <CardGroup className="homeCard">
+          {/* <Nav className="flex-column">
+            <Nav.Link>
+              <AiFillLinkedin
+                onClick={() =>
+                  window.open("https://www.linkedin.com/in/aswathyeg/")
+                }
+                color="black"
+              />
+            </Nav.Link>
+            <Nav.Link eventKey="link-1">
+              <AiTwotoneMail color="black" />
+            </Nav.Link>
+            <Nav.Link eventKey="link-2">
+              <AiFillGithub
+                color="black"
+                onClick={() => window.open("https://github.com/aswathyeg")}
+              />
+            </Nav.Link>
+          </Nav> */}
           <Card className="homeCard">
             <Card.Body>
               <Card.Title className="cardTitle">Aswathy E G</Card.Title>
+
               {/* <MoreAbout /> */}
               <Card.Text>
                 Hi, I'm a front end developer. Over the past 2 years experience
                 in development, I have done a handful of projects in React.
-                <br></br> I am doing reguler cotributions to Github.Sharing my
-                technical knowledge in Medium is my other interest.
+                {/* <br></br> I am doing reguler cotributions to Github.Sharing my
+                technical knowledge in Medium is my other interest. */}
               </Card.Text>
             </Card.Body>
             {/* <div>
@@ -69,7 +95,17 @@ const Home = () => {
             </Figure>
           </Card>
         </CardGroup>
+
+        {/* <div>
+          <button className="btn-scroll" onClick={handleClickScroll}>
+            Scroll Down
+          </button>
+        </div>
+        <div id="section-1">
+          <About />
+        </div> */}
         <About />
+
         <Works />
       </Container>
     </div>
