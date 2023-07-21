015b163296513fc0a4c0a3393a222bc4966b1ae1
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-21 17:21:33 +0530

    added progress bar

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,15 +1,84 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ProgressBar from "react-bootstrap/ProgressBar";
+import Card from "react-bootstrap/Card";
 const Skills = () => {
-  const percentage = 70;
+  const htmlPercentage = 70;
+  const cssPercentage = 60;
+  const javascriptPercentage = 65;
+  const reactPercentage = 70;
+  const azurePercentage = 10;
+  const bootstrapPercentage = 50;
+  const cypressPercentage = 60;
   return (
     <div>
-      <Container className="subComponentsContainer">
+      <Card.Title className="titleCard">Skills</Card.Title>
+      <Container className="progressbarContainer">
         <div className="progressBar">
           <ProgressBar
-            now={percentage}
-            label={` HTML ${percentage}% `}
+            now={htmlPercentage}
+            label={` HTML ${htmlPercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={cssPercentage}
+            label={` CSS ${cssPercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={javascriptPercentage}
+            label={` JavaScript ${javascriptPercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={reactPercentage}
+            label={` React ${reactPercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={azurePercentage}
+            label={` Azure ${azurePercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={bootstrapPercentage}
+            label={` Bootstrap ${bootstrapPercentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+      <Container className="progressbarContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={cypressPercentage}
+            label={` Cypress ${cypressPercentage}% `}
             variant="dark"
             animated
           />
