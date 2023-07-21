ebfe278cacbcf6d59b54e196e7cba9b1c6221bc3
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-21 15:40:49 +0530

    added progressbar

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
 const Skills = () => {
-  return <div>Skills</div>;
+  const percentage = 70;
+  return (
+    <div>
+      <Container className="subComponentsContainer">
+        <div className="progressBar">
+          <ProgressBar
+            now={percentage}
+            label={` HTML ${percentage}% `}
+            variant="dark"
+            animated
+          />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default Skills;
