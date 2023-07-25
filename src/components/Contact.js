30a6e134b0813286cf1a1a8220d761451c316199
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-25 17:43:30 +0530

    clean up classname

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import Aswathy_EG_Resume from "../assets/resume.pdf";
+import { Link } from "react-router-dom";
 const Contact = () => {
   return (
     <div>
-      Contact
-      {/* download CV */}
+      <Container className="subComponentsContainer">
+        {/* download CV */}
+        <h4>Download Resume</h4>
+        <Link
+          to={Aswathy_EG_Resume}
+          download="Example-PDF-document"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aswathy E G_Resume
+        </Link>
+      </Container>
     </div>
   );
 };
