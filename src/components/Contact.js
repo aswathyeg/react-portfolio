6d0e8f13b2820d7b889e4933e00e3c485157faf7
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-25 21:32:52 +0530

    added form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,22 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Aswathy_EG_Resume from "../assets/resume.pdf";
 import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 const Contact = () => {
   return (
     <div>
       <Container className="subComponentsContainer">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+
         {/* download CV */}
         <h4>Download Resume</h4>
         <Link
