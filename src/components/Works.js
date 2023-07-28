a591f906f14c33053b20bd5dd5a51f507c3ceaf6
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-28 16:49:51 +0530

    cards added

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,66 +1,67 @@
-import { Container } from "react-bootstrap";
-import Card from "react-bootstrap/Card";
-import Row from "react-bootstrap/Row";
-
+import { Card, Container, CardGroup } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
-import Carousel from "react-bootstrap/Carousel";
+import Row from "react-bootstrap/Row";
 import bodhii from "../assets/bodhii.jpg";
-import githubScreenshot from "../assets/githubScreenshot.jpg";
-const Works = (props) => {
+import ListGroup from "react-bootstrap/ListGroup";
+function Works(props) {
   return (
-    <div className={!props.text ? "homeContainer" : "null"}>
-      <Container className="subComponentsContainer">
+    <Container>
+      <Card.Body>
+        {" "}
         {props.text ? (
           <Card.Title className="titleCard">Works</Card.Title>
         ) : null}
-        <Carousel
-          interval={null}
-          variant="dark"
-          className="carouselClass"
-          indicators={false}
-        >
-          <Carousel.Item>
-            <img className="d-block w-100" src={bodhii} alt="First slide" />
-            {/* <Carousel.Caption className="WorksCarousal">
-            <h3>Bodhii</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption> */}
-          </Carousel.Item>
-          <Carousel.Item interval={100000}>
-            <img className="d-block w-100" src={bodhii} alt="Second slide" />
-            {/* <Carousel.Caption className="WorksCarousal">
-            <h3>Papyrus</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption> */}
-          </Carousel.Item>
-          <Carousel.Item interval={100000}>
-            <img className="d-block w-100" src={bodhii} alt="Third slide" />
+      </Card.Body>
+      <CardGroup className="homeCard">
+        <Card className="homeCard"></Card>
+        <div className="about">
+          <div class="m-4">
+            <Card className="worksCard1">
+              {/* <BsBriefcaseFill /> */}
+              <Card.Body>
+                <Card.Title>Bodhii</Card.Title>
+                {/* <Card.Text
+                  className={props.text ? "homeSmallCards" : "smallCards"}
+                >
+                  2+ years
+                </Card.Text> */}
+              </Card.Body>
+              {/* <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer> */}
+            </Card>
+          </div>
+          <div class="m-4">
+            <Card className="worksCard1">
+              {/* <HiAcademicCap /> */}
 
-            {/* <Carousel.Caption className="WorksCarousal">
-            <h3>Shopping </h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
-          </Carousel.Item>
-          <Carousel.Item interval={100000}>
-            <img
-              className="d-block w-100"
-              src={githubScreenshot}
-              alt="Fourth slide"
-            />
-
-            {/* <Carousel.Caption className="WorksCarousal">
-            <h3>Shopping </h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption> */}
-          </Carousel.Item>
-        </Carousel>
-      </Container>
-    </div>
+              <Card.Body>
+                <Card.Title>Papyrus</Card.Title>
+                {/* <Card.Text
+                  className={props.text ? "homeSmallCards" : "smallCards"}
+                >
+                  MCA
+                </Card.Text> */}
+              </Card.Body>
+              {/* <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer> */}
+            </Card>
+          </div>
+          {/* <div class="m-4">
+            <Card className="card1">
+             
+              <Card.Body>
+                <Card.Title>Othanachan</Card.Title>
+                
+              </Card.Body>
+             
+            </Card>
+          </div> */}
+        </div>
+      </CardGroup>
+    </Container>
   );
-};
+}
 
 export default Works;
