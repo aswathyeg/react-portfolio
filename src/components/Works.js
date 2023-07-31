81f3b297d9f541020515e8013a6f39e94fc770e4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-07-31 11:12:44 +0530

    updated cards

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,82 +1,72 @@
-import { Card, Container, CardGroup } from "react-bootstrap";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
+import React, { useState } from "react";
+import { Container, Card, CardGroup, Figure } from "react-bootstrap";
+import aswathy from "../assets/aswathy.jpg";
+import { BsBriefcaseFill } from "react-icons/bs";
+import { HiAcademicCap } from "react-icons/hi";
+import { HiSupport } from "react-icons/hi";
 import bodhii from "../assets/bodhii.jpg";
 import excercise from "../assets/excercise.jpg";
-import ListGroup from "react-bootstrap/ListGroup";
-import githubScreenshot from "../assets/githubScreenshot.jpg";
-function Works(props) {
+const About = (props) => {
   return (
-    <Container>
-      <Card.Body>
-        {" "}
-        {props.text ? (
-          <Card.Title className="titleCard">Works</Card.Title>
-        ) : null}
-      </Card.Body>
-      <CardGroup className="homeCard">
-        <Card className="homeCard"></Card>
-        <div className="about">
-          <div class="m-4">
-            <Card className="worksCard1">
-              {/* <BsBriefcaseFill /> */}
-              <Card.Body>
-                <Card.Title>Bodhii</Card.Title>
-                <Card.Img src={bodhii}></Card.Img>
-                <Card.Text
-                  className={props.text ? "homeSmallCards" : "smallCards"}
-                >
-                  An E-learning platform
-                </Card.Text>
-              </Card.Body>
-              {/* <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer> */}
-            </Card>
-          </div>
-
-          <div class="m-4">
-            <Card className="worksCard1">
-              {/* <HiAcademicCap /> */}
-
-              <Card.Body>
-                <Card.Title>Excercise</Card.Title>
-                <Card.Img src={excercise}></Card.Img>
-              </Card.Body>
-              {/* <Card.Footer>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </Card.Footer> */}
-            </Card>
-          </div>
-          {/* <div class="m-4">
-            <Card className="card1">
-             
-              <Card.Body>
-                <Card.Title>Othanachan</Card.Title>
-                
-              </Card.Body>
-             
-            </Card>
-          </div> */}
-        </div>
-      </CardGroup>
-      <br></br>
-      <div className="gitCard">
-        <Card className=" text-white">
-          <Card.Img src={githubScreenshot} alt="Card image" />
-          <Card.ImgOverlay>
-            <Card.Title>Card title</Card.Title>
-            {/* <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </Card.Text> */}
-            {/* <Card.Text>Last updated 3 mins ago</Card.Text> */}
-          </Card.ImgOverlay>
+    <div>
+      <Container className="subComponentsContainer">
+        <Card className="homeCard">
+          <Card.Body>
+            {props.text ? (
+              <Card.Title className="titleCard">Works</Card.Title>
+            ) : null}
+          </Card.Body>
         </Card>
-      </div>
-    </Container>
-  );
-}
 
-export default Works;
+        <CardGroup className="homeCard">
+          {/* <Card.Img variant="top" roundedCi src={photo} alt="my profile" /> */}
+
+          <div className="about">
+            <div class="m-4">
+              <Card className="worksCard1">
+                <Card.Body>
+                  <Card.Title>Bodhii</Card.Title>
+                  <Card.Img src={bodhii} />
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="about">
+            <div class="m-4">
+              <Card className="worksCard1">
+                <Card.Body>
+                  <Card.Title>Food Order App</Card.Title>
+                  <Card.Img src={bodhii} />
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="about">
+            <div class="m-4">
+              <Card className="worksCard1">
+                <Card.Body>
+                  <Card.Title>Othanachan</Card.Title>
+                  <Card.Img src={bodhii} />
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+
+          <div className="about">
+            <div class="m-4">
+              <Card className="worksCard1">
+                <Card.Body>
+                  <Card.Title>Papyrus</Card.Title>
+                  <Card.Img src={bodhii} />
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </CardGroup>
+      </Container>
+    </div>
+  );
+};
+export default About;
