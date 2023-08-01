2d6641954fbcddb3fa2beee3b6a07e7a7bd2328b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-01 13:10:25 +0530

    for profile photo

--- src/components/WorksNew.js
+++ src/components/WorksNew.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./WorksNew.css";
-
+import aswathy from "../assets/aswathy.jpg";
 const WorksNew = () => {
-  return <div>WorksNew</div>;
+  return (
+    <div className="allWorksNew">
+      <p class="browser-warning">
+        If this looks wonky to you it's because this browser doesn't support the
+        CSS property 'aspect-ratio'.
+      </p>
+      <div class="stack">
+        <div class="cardWorksNew">
+          <div class="image">
+            <img src={aswathy} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WorksNew;
