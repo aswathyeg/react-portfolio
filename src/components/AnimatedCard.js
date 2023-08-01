01b21d6d75f32cbaa78c5712a64ce0df43525a82
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-01 13:04:58 +0530

    added event handling to card

--- src/components/AnimatedCard.js
+++ src/components/AnimatedCard.js
@@ -3,6 +3,8 @@ import "./AnimatedCard.css";
 import useReducer from "../assets/useReducer.jpg";
 import routing from "../assets/routing.jpg";
 import chart from "../assets/chart.jpg";
+import cypressIntro from "../assets/cypressIntro.jpg";
+import cypress from "../assets/cypress.jpg";
 import statemanagement from "../assets/statemanagement.jpg";
 import { Container, Card, CardGroup, Figure } from "react-bootstrap";
 const AnimatedCard = () => {
@@ -13,7 +15,14 @@ const AnimatedCard = () => {
         <br></br>
         <body>
           <div class="box">
-            <div class="animatedCard">
+            <div
+              class="animatedCard"
+              onClick={() =>
+                window.open(
+                  "https://aswathyeg.medium.com/using-chart-js-in-react-239d0c7f055c"
+                )
+              }
+            >
               <div class="imgBx">
                 <img src={chart} alt="images" />
               </div>
@@ -42,6 +51,37 @@ const AnimatedCard = () => {
               <div class="details">
                 <h2>
                   SomeOne Famous<span>Actor</span>
+                </h2>
+              </div>
+            </div>
+
+            <div class="animatedCard">
+              <div class="imgBx">
+                <img src={routing} alt="images" />
+              </div>
+              <div class="details">
+                <h2>
+                  SomeOne Famous<span>Producer</span>
+                </h2>
+              </div>
+            </div>
+            <div class="animatedCard">
+              <div class="imgBx">
+                <img src={cypressIntro} alt="images" />
+              </div>
+              <div class="details">
+                <h2>
+                  SomeOne Famous<span>Producer</span>
+                </h2>
+              </div>
+            </div>
+            <div class="animatedCard">
+              <div class="imgBx">
+                <img src={cypress} alt="images" />
+              </div>
+              <div class="details">
+                <h2>
+                  SomeOne Famous<span>Producer</span>
                 </h2>
               </div>
             </div>
