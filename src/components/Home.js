aa997e1152a4fa9fa5e10acba5f5be925fd94d87
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-03 22:46:23 +0530

    removed skills component

--- src/components/Home.js
+++ src/components/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
         <AnimatedCard />
 
         {/* <WorksNew /> */}
-        <Skills />
+        {/* <Skills /> */}
         {/* <Writings /> */}
 
         {/* <GitHubContribution /> */}
