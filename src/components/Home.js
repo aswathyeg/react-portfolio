cc00dc958139621b9e40ac17bd5456f07c20e61f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-08-03 17:16:12 +0530

    text and icons updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,47 +23,21 @@ import WorksNew from "./WorksNew";
 const Home = () => {
   const [cardText, setCardText] = useState(true);
 
-  // const handleClickScroll = () => {
-  //   const element = document.getElementById("section-1");
-  //   if (element) {
-  //     // 👇 Will scroll smoothly to the top of the next section
-  //     element.scrollIntoView({ behavior: "smooth" });
-  //   }
-  // };
   return (
     <div>
-      <Nav className="flex-column">
-        {/* <Nav.Link>
-          <AiFillLinkedin
-            onClick={() =>
-              window.open("https://www.linkedin.com/in/aswathyeg/")
-            }
-            color="black"
-          />
-        </Nav.Link>
-        <Nav.Link eventKey="link-1">
-          <BsMedium
-            onClick={() => window.open("https://aswathyeg.medium.com/")}
-            color="black"
-          />
-        </Nav.Link>
-        <Nav.Link eventKey="link-2">
-          <AiFillGithub
-            color="black"
-            onClick={() => window.open("https://github.com/aswathyeg")}
-          />
-        </Nav.Link> */}
-      </Nav>
-      <Container className="homeContainer">
-        <CardGroup className="homeCard">
-          <Card className="homeCard">
-            <h4 className="heroText">Aswathy</h4>
-            <Card.Title className="homeTitle"> UI Developer</Card.Title>
-
-            <Card.Body className="homeCard">
+      {/* <Nav className="flex-column"></Nav> */}
+      <Container>
+        <CardGroup>
+          <Card className="descriptionCard">
+            <div className="heroText">
+              <h4 className="heroText">Aswathy</h4>
+              <h className="homeTitle"> UI Developer</h>
+            </div>
+            <Card.Body className="descriptionCard">
               <Nav className="flex-column">
                 <Nav.Link>
                   <AiFillLinkedin
+                    size={40}
                     onClick={() =>
                       window.open("https://www.linkedin.com/in/aswathyeg/")
                     }
@@ -72,12 +46,14 @@ const Home = () => {
                 </Nav.Link>
                 <Nav.Link eventKey="link-1">
                   <BsMedium
+                    size={40}
                     onClick={() => window.open("https://aswathyeg.medium.com/")}
                     color="black"
                   />
                 </Nav.Link>
                 <Nav.Link eventKey="link-2">
                   <AiFillGithub
+                    size={40}
                     color="black"
                     onClick={() => window.open("https://github.com/aswathyeg")}
                   />
@@ -92,7 +68,7 @@ const Home = () => {
                 I am a Bangalore based Front End Developer.
                 <br></br> Interested in developing impactful User Interfaces
                 that speak volumes.
-                <br></br> Let's Work Together.
+                <br></br> Let's work together.
               </Card.Text>
             </Card.Body>
           </Card>
@@ -111,9 +87,9 @@ const Home = () => {
             </Card.Body>
           </Card>
         </CardGroup>
+        <Works text={cardText} />
         <AnimatedCard />
 
-        <Works text={cardText} />
         {/* <WorksNew /> */}
         <Skills />
         {/* <Writings /> */}
