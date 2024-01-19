833b4673667f73324442a6f1ef171e6d91075a9e
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-19 20:18:00 +0530

    added projects

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Works from "./components/Works";
 import Writings from "./components/Writings";
 import Footer from "./components/Footer";
 import AnimatedCard from "./components/AnimatedCard";
+import Projects from "./components/Projects";
+
 
 // import "./App.css";
 function App() {
@@ -22,7 +24,7 @@ function App() {
         <Route path="works" element={<Works />} />
         <Route path="writings" element={<AnimatedCard />} />
         <Route path="contact" element={<Contact />} />
-   
+        <Route path="projects" element={<Projects />} />
       </Routes>
       <Footer className="footerClass" />
     </div>
