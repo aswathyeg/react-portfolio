7aa3ff3133d02f122e2f40e09fcacad40816ec4c
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-19 20:24:21 +0530

    navbar

---
+++ src/components/TourNavBar.js
@@ -0,0 +1,39 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import "./TourNavBar.css";
+import { useState } from "react";
+const TourNavBar = () => {
+  const [navbar, setNavbar] = useState(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+  window.addEventListener("scroll", changeBackground);
+  return (
+    <Navbar className={navbar ? "navbar active" : "navbar"}>
+      <Container className="containerClass">
+        <Navbar.Brand
+          href="/"
+          //
+          className="navBrand"
+        >
+          Explore Hampi
+        </Navbar.Brand>
+
+        <Nav className="navbarHome">
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/location">Location</Nav.Link>
+          <Nav.Link href="/plan">Plan</Nav.Link>
+          <Nav.Link href="/stay">Stay</Nav.Link>
+          <Nav.Link href="/books">Books</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+export default TourNavBar;
