da26c4504872871222f2bd7fd29d9d545b332e9c
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-01-29 17:58:38 +0530

    updated classname

--- src/components/TourNavBar.js
+++ src/components/TourNavBar.js
@@ -5,7 +5,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import "./TourNavBar.css";
 import { useState } from "react";
 const TourNavBar = () => {
-  const [navbar, setNavbar] = useState(false);
+  const [tournavbar, setNavbar] = useState(false);
   const changeBackground = () => {
     if (window.scrollY >= 80) {
       setNavbar(true);
@@ -15,17 +15,17 @@ const TourNavBar = () => {
   };
   window.addEventListener("scroll", changeBackground);
   return (
-    <Navbar className={navbar ? "navbar active" : "navbar"}>
+    <Navbar className={tournavbar ? "tournavbar active" : "tournavbar"}>
       <Container className="containerClass">
         <Navbar.Brand
           href="/"
           //
-          className="navBrand"
+          className="tournavBrand"
         >
           Explore Hampi
         </Navbar.Brand>
 
-        <Nav className="navbarHome">
+        <Nav className="tournavbarHome">
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/location">Location</Nav.Link>
           <Nav.Link href="/plan">Plan</Nav.Link>
